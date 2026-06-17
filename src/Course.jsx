a3086634062d67f2f8a4626a10ecbd667216ff5f
Athisankar-2006 <athisankar2k6@gmail.com>
2026-06-17 17:20:25 +0530

    onclick

--- src/Course.jsx
+++ src/Course.jsx
@@ -2,13 +2,17 @@ import CRS from "./assets/CRS.png"
 
 function Course(props){
 
+    function check(discount){
+        console.log( props.name +"clicked ," +discount +"% discount")
+    }
+
     return(
 
        props.name && <div className="cart">
                 <img src={props.image} alt=""/>
                 <h3>name : {props.name}</h3>
                 <p> price : {props.price} </p>
-                <p> rating : {props.rating} </p>
+                <button onClick={()=>check(20)}>click</button>
                 
         </div>
     );
